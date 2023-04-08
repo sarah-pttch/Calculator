2de101ef9abcfe4e2d2b92bb8ce45b8aae2adc5d
sarah-pttch <sarah.pttch@gmail.com>
2023-04-08 23:33:10 +0200

    changed String to Integer

--- src/main/java/com/example/calculator/Controller/CalculatorController.java
+++ src/main/java/com/example/calculator/Controller/CalculatorController.java
@@ -1,14 +1,18 @@
 package com.example.calculator.Controller;
 
 import org.springframework.web.bind.annotation.*;
+import java.util.*;
 
 @RestController
 public class CalculatorController{
 
 
     @GetMapping("/sum")
-    public int sum(@RequestParam String numbers) {
-        String[] sub = numbers.split(",");
-        return Integer.parseInt(sub[0]) + Integer.parseInt(sub[1]);
+    public int sum(@RequestParam List<Integer> numbers) {
+        Integer sum = 0;
+        for(Integer number : numbers){
+            sum += number;
+        }
+        return sum;
     }
 }
