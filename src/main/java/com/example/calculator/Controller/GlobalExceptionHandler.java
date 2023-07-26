fafd67db13692af5e80c1ef7f4dba2c6aca6825c
sarah-pttch <sarah.pttch@gmail.com>
2023-07-27 00:10:12 +0200

    add return invalid values

--- src/main/java/com/example/calculator/Controller/GlobalExceptionHandler.java
+++ src/main/java/com/example/calculator/Controller/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.example.calculator.Controller;
 
+import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
+import java.util.ArrayList;
+import java.util.List;
 
 
 @ControllerAdvice
@@ -18,7 +21,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ConstraintViolationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     ResponseEntity<String>
-    handleConstraintViolationException(Exception e){
+    handleConstraintViolationException(ConstraintViolationException e){
 //        String message = e.getMessage();
 //        int first = message.indexOf("[");
 //        int second = message.lastIndexOf("[");
@@ -29,13 +32,18 @@ public class GlobalExceptionHandler {
 //            return new ResponseEntity<>("Both numbers invalid: must be greater than or equal to zero", HttpStatus.BAD_REQUEST);
 //        }
 
-        Object errorOne = ((ConstraintViolationException) e).getConstraintViolations().iterator().next().getInvalidValue();
-        if(((ConstraintViolationException) e).getConstraintViolations().size() == 2) {
-            Object errorTwo = ((ConstraintViolationException) e).getConstraintViolations().iterator().next().getInvalidValue();
-            return new ResponseEntity<>("Invalid input: " + errorOne + ", " + errorTwo + ". Must be greater than or equal to zero.", HttpStatus.BAD_REQUEST);
+//        Object errorOne = e.getConstraintViolations().iterator().next().getInvalidValue();
+//        if(e.getConstraintViolations().size() == 2) {
+//            Object errorTwo = e.getConstraintViolations().iterator().next().getInvalidValue();
+//            return new ResponseEntity<>("Invalid input: " + errorOne + ", " + errorTwo + ". Must be greater than or equal to zero.", HttpStatus.BAD_REQUEST);
+//        }
+
+        List<Object> negativeNumbers = new ArrayList<>();
+        for(ConstraintViolation cv : e.getConstraintViolations()){
+            negativeNumbers.add(cv.getInvalidValue());
         }
 
-        return new ResponseEntity<>("Invalid input: " + errorOne + ". Must be greater than or equal to zero.", HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>("Invalid input: " + negativeNumbers + ". Must be greater than or equal to zero.", HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
